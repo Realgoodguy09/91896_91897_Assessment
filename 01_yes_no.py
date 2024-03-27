4941d62bc8f29b63f25e77c3a32536c06ac77852
Jackson <21115@fraser.school.nz>
2024-03-28 12:35:20 +1300

    28th march

---
+++ 01_yes_no.py
@@ -0,0 +1,39 @@
+# used to print spaces each restart / run
+print()
+print()
+print()
+print()
+
+# function will go here
+
+# checks user enters a valid response
+def yes_no(question):
+
+
+    while True:
+        response = input(question).lower()
+
+        # if answer is yes, returns yes
+        if response == 'yes' or response == 'y':
+            return 'yes'
+
+        # if answer is no, returns no
+        elif response == 'no' or response == 'n':
+            return 'no'
+
+        # loops and asks for a valid response
+        else:
+            print("Please enter yes / no")
+
+# main routine goes here
+
+
+cheese = yes_no("Do you like cheese? ")
+
+if cheese == "yes":
+    print("Noice")
+
+else:
+    print("Shame on you.")
+
+print("cheese")
